4eb7bbfe731d40f5b7c662e1c45988fe25cb4533
Favio33 <favio.varillas36@gmail.com>
2023-05-13 23:36:32 -0500

    First Commit

---
+++ src/bin/variables.py
@@ -0,0 +1,17 @@
+import os
+
+# Set Environment Variables
+os.environ['env'] = 'TEST'
+os.environ['header'] = 'True'
+os.environ['inferSchema'] = 'True'
+
+# Ger Environment Variables
+env = os.environ['env']
+header = os.environ['header']
+inferSchema = os.environ['inferSchema']
+
+# Set Other Variables
+appName = "USA Prescriber Research Report"
+currentPath = os.getcwd()
+pathStagingDimensionCity = currentPath + '\\staging\\dimension_city'
+pathStagingFact = currentPath + '\\staging\\fact'
